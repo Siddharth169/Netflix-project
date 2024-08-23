8f0a341ed7fa97e26ba6cedbd3139340fa9e089d
Siddharth <100271223+Siddharth169@users.noreply.github.com>
2024-08-23 17:27:36 +0530

    Add files via upload

--- Project-netflix/main.tf
+++ Project-netflix/main.tf
@@ -7,6 +7,86 @@ variable "ami_id" {
   description = "Enter the requested ami id"
 }
 
+resource "aws_vpc" "netflix-vpc" {
+  cidr_block = "10.0.0.0/16"  
+  tags = {
+    Name = "Netflix"
+  }
+}
+
+/*resource "aws_subnet" "private-subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.netflix-vpc.id
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Private"
+  }
+}*/
+
+resource "aws_subnet" "public-subnet" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id = aws_vpc.netflix-vpc.id
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_internet_gateway" "netflix-igw" {
+  vpc_id = aws_vpc.netflix-vpc.id
+  tags = {
+    Name = "Netflix_igw"
+  }
+}
+
+resource "aws_route_table" "netflix-rt" {
+  vpc_id = aws_vpc.netflix-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.netflix-igw.id
+  }
+}
+
+resource "aws_route_table_association" "netflix-rts" {
+  subnet_id = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.netflix-rt.id
+}
+
+resource "aws_security_group" "jenkins-server-sg" {
+  vpc_id = aws_vpc.netflix-vpc.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol = "tcp"
+    from_port = 22
+    to_port = 22
+  }
+  
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol = "tcp"
+    from_port = 8080
+    to_port = 8080
+    description = "Jenkins application"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol = "tcp"
+    from_port = 9000
+    to_port = 9000
+    description = "sonarqube"
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol = -1
+    from_port = 0
+    to_port = 0
+  }
+  tags = {
+    Name = "Jenkins-server-sg"
+  }
+}
+
 resource "aws_key_pair" "Jenkins-key" {
   key_name = "Jenkins-key"
   public_key = file("~/.ssh/ec2.pub")
@@ -16,10 +96,12 @@ resource "aws_instance" "Jenkins" {
   ami = var.ami_id
   instance_type = "t3.medium"
   key_name = aws_key_pair.Jenkins-key.key_name
+  vpc_security_group_ids = ["aws_security_group.jenkins-server-sg.id"]
+  subnet_id = aws_subnet.public-subnet.id
   tags = {
     Name = "Jenkins-server"
   }
-  user_data = file("./jenkins-installation.sh")
+  user_data = file("./jenkins-installation.sh") #we can also use provisioner (remote-exec) to deploy the service we want to installing the resource creating
 }
 
 output "public_ip" {
